52762cb0a7744fe2ff3b18e26abb4b2f0af90891
BeagleBoard <beagle@beagleboard.org>
2021-01-17 19:55:12 -0500

    implemented tensorflow js and google translate api

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Project-Iris
 
 An application that translates objects you see into words in your language. 
+
+# Setup
+
+Set an environment (GOOGLE_APPLICATION_CREDENTIALS) variable to the cloud key json file:
+
+-Windows: set GOOGLE_APPLICATION_CREDENTIALS="./project-iris-cloud-key.json"
+-Linux/macOS: export GOOGLE_APPLICATION_CREDENTIALS="./project-iris-cloud-key.json"
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express'); 
 const app = express();
-const translate = require('google-translate-api');
+const {Translate} = require('@google-cloud/translate').v2;
 const { parseURL, basicURLParse } = require('whatwg-url');
 const path = require('path');
+const translate = new Translate();
+
 
 const PORT = 8080;
 
@@ -21,17 +23,29 @@ app.post('/translate', async (req, res) => {
     parsed_json = JSON.parse(req.body);
     const text = parsed_json['text'];
     const lang = parsed_json['lang'];
-    res.writeHead(200,{'ContentType':'text/plain'});
+    // res.writeHead(200,{'ContentType':'text/plain'});
+    // console.log('text: '+text);
+    // console.log('lang: '+lang);
     try{
-        translate_resp = await translate(text, {from: 'en', to: lang})
-        res.write(translate_resp.text);
+        let [translations] = await translate.translate(text, lang);
+        translations = Array.isArray(translations) ? translations : [translations];
+        res.send(translations);
     }
     catch(err){
         console.log("Oops an error ocurred with getting a response from Google translate");
         console.log(err);
-        res.write("error");
+        res.send("error");
     }
-    res.end();
+    // try{
+    //     translate_resp = await translate(text, {from: 'en', to: lang})
+    //     res.write(translate_resp.text);
+    // }
+    // catch(err){
+    //     console.log("Oops an error ocurred with getting a response from Google translate");
+    //     console.log(err);
+    //     res.write("error");
+    // }
+    // res.end();
 })
 
 app.listen(PORT, ()=>{
